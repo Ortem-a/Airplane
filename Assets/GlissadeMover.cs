3550aded1775237d151d08921b27d35be8822a0a
Artyom <60854544+Ortem-a@users.noreply.github.com>
2022-09-26 18:34:23 +0300

    Реализована первая версия движения по глиссаде

--- Assets/GlissadeMover.cs
+++ Assets/GlissadeMover.cs
@@ -7,8 +7,10 @@ public class GlissadeMover : MonoBehaviour
     public float Theta = 2f; // угол наклона глиссады, градусы
     public float HorizontalVelocity = 69.444f; // начальная скорость, м/с
     public float StartDistance = 10000f; // начальное расстояние до ВПП, м
-    public float StartAlignmentHeight = 500f; // высота в точке начала выравнивания, м
-    public float VerticalVelocity = 5f;
+    public float StartAlignmentHeight = 400f; // высота в точке начала выравнивания, м
+    public float VerticalVelocity; // вертикальная компонента скорости, м
+    public float ChangeTrackHeight = 20f; // высота, при которой изменяется траектория движения, м
+    public float VerticalVelocity2 = 5f; // вертикальная компонента скорости, м
 
     private Transform _t;
     private float _tgTheta;
@@ -18,59 +20,72 @@ public class GlissadeMover : MonoBehaviour
     private void Start()
     {
         _t = GetComponent<Transform>();
-        _t.position = new Vector3(StartDistance, StartAlignmentHeight, 0);
+        _t.position = new Vector3(0, StartAlignmentHeight, 0);
         _tgTheta = Mathf.Tan(Theta * Mathf.PI / 180);
         VerticalVelocity = HorizontalVelocity * _tgTheta;
-
-        for (int s = 0; s < _numberOfPoint; s++)
-        {
-             _points[s] = new Vector3(
-                -15 * Mathf.Exp(-1 * Mathf.Tan(Theta) * s / 15),
-                - s + 15, 0);
-        }
+        StartAlignmentHeight = _tgTheta * StartDistance;
+        DrawTrack();
     }
 
     private void Update()
     {
-        if (_t.position.y > 15)
+        if (_t.position.y > 10)
         {
-            Vector3 point = new Vector3(-HorizontalVelocity, -VerticalVelocity, 0);
-            Debug.DrawLine(new Vector3(StartDistance, StartAlignmentHeight, 0), _t.position, Color.blue);
-            _t.position += point * Time.deltaTime * 100;
+            Vector3 point = new Vector3(HorizontalVelocity, -VerticalVelocity, 0);
+            Debug.DrawLine(new Vector3(0, StartAlignmentHeight, 0), _t.position, Color.blue);
+            _t.position += point * Time.deltaTime * 20;
+        }
+        else if (_t.position.y <= 10 && _t.position.y >= 1)
+        {
+            // кривая от высоты 10 до высоты 1
+            Vector3 point = new Vector3(HorizontalVelocity, -VerticalVelocity2, 0);
+            _t.position += point * Time.deltaTime;
+            HorizontalVelocity -= 0.1f;
+            VerticalVelocity2 -= 0.01f;
         }
         else
         {
-            if (_t.position.y <= 10 && HorizontalVelocity != 0f) // коснулись земли
-            {
-                VerticalVelocity = 0f;
-                HorizontalVelocity -= 10f * Time.deltaTime;
-            }
-
-            for (int i = 0; i < _numberOfPoint-1; i++)
-            {
-                Debug.DrawLine(_points[i], _points[i+1], Color.red, 10f);
-            }
-
-            if (HorizontalVelocity <= 0f)
-            {
-                HorizontalVelocity = 0f;
-            }
-
-            Vector3 point = new Vector3(-HorizontalVelocity, -VerticalVelocity, 0);
+            Vector3 point = new Vector3(HorizontalVelocity, -VerticalVelocity2, 0);
             _t.position += point * Time.deltaTime;
+            VerticalVelocity2 -= 0.1f;
+            HorizontalVelocity -= 0.1f;
         }
-    }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        for (int s = 0; s < _numberOfPoint; s++)
+        if (HorizontalVelocity <= 0f)
         {
-            Gizmos.DrawSphere(new Vector3(
-               -15 * Mathf.Exp(-1 * Mathf.Tan(Theta) * s / 15),
-               - s + 15, 0), 1f);
+            HorizontalVelocity = 0f;
         }
-
-        Gizmos.DrawLine(new Vector3(StartDistance, StartAlignmentHeight, 0), new Vector3(-3.8887f, 15f, 0));
+        if (VerticalVelocity2 <= 0f)
+        {
+            VerticalVelocity2 = 0f;
+        }
+        if (_t.position.y <= 0f)
+        {
+            _t.position = new Vector3(_t.position.x, 0f, 0f);
+        }
     }
+
+    private void DrawTrack()
+    {
+        Vector3 previousPoint = new Vector3(0, Line1(0), 0);
+        Vector3 point;
+        for (int x = 1; x < 100_000; x++)
+        {
+            if (Line1(x) > ChangeTrackHeight)
+            {
+                point = new Vector3(x, Line1(x), 0);
+                Debug.DrawLine(previousPoint, point, Color.green, 100f);
+            }
+            else
+            {
+                point = new Vector3(x, Curve(x), 0);
+                Debug.DrawLine(previousPoint, point, Color.green, 100f);
+            }
+            previousPoint = point;
+        }
+    }
+
+    private float Line1(float x) => -_tgTheta * x + StartAlignmentHeight;
+    private float Line2(float x) => -Mathf.Tan(1 * Mathf.PI / 180) * x + StartAlignmentHeight;
+    private float Curve(float x) => ChangeTrackHeight * Mathf.Exp(-1 * Mathf.Tan(Theta) * x / ChangeTrackHeight);
 }
